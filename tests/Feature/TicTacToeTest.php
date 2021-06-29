5f828207240c0ca0335f0228dc614556260068d0
Michal Ardziejewski <adjweb@gmail.com>
2021-06-29 11:11:13 +0200

    Add more tests

--- tests/Feature/TicTacToeTest.php
+++ tests/Feature/TicTacToeTest.php
@@ -111,6 +111,33 @@ class TicTacToeTest extends TestCase
             );
     }
 
+    public function test_tic_tac_toe_store_api_406_request(): void
+    {
+        $response = $this->postJson('/api/o', ['x' => 1, 'y' => 2]);
+
+        $response
+            ->assertStatus(406)
+            ->assertJson(
+                [
+                    'message' => 'Piece is being placed out of turn.',
+                ],
+            );
+    }
+
+    public function test_tic_tac_toe_store_api_409_request(): void
+    {
+        $response = $this->postJson('/api/x', ['x' => 1, 'y' => 2]);
+        $response->assertStatus(200);
+
+        $responseSecond = $this->postJson('/api/x', ['x' => 1, 'y' => 2]);
+        $responseSecond->assertStatus(409)
+                       ->assertJson(
+                           [
+                               'message' => 'Piece is being placed where a piece already is.',
+                           ],
+                       );
+    }
+
     public function test_tic_tac_toe_destroy_api_request(): void
     {
         $response = $this->deleteJson('/api/');
